6bcdbda4a235ea9212910c4bc151686b53286a20
DDC <diaz_carrete@yahoo.com>
2015-01-16 13:13:46 -0500

    basic alternatives

--- src/Main.hs
+++ src/Main.hs
@@ -5,5 +5,27 @@ module Main (main) where
 import BasePrelude
 import MTLPrelude
 
+import qualified Options.Applicative as O
+
+data Command = 
+    Example
+  | Query
+  | Report
+  | Cat
+  | Collate
+  | Diff
+  | Pretty 
+  deriving (Show)   
+
+complete p desc = O.info (O.helper <*> p) 
+                         (O.fullDesc <> O.progDesc desc)
+
+theParser :: O.Parser Command 
+theParser = (O.subparser . mconcat)
+    [ O.command "example" (complete (pure Example) "Generate example plan")
+    , O.command "query" (complete (pure Query) "Perform queries") ]
+
 main :: IO ()
-main = print "" 
+main = do
+    O.execParser (complete theParser "This is the main prog desc")
+    print "" 
