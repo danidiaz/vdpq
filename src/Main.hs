254bed6ab47a8c53341e2c99d51001a1aa361591
DDC <diaz_carrete@yahoo.com>
2015-01-16 13:43:15 -0500

    renamings

--- src/Main.hs
+++ src/Main.hs
@@ -17,24 +17,25 @@ data Command =
   | Pretty 
   deriving (Show)   
 
-complete :: O.Parser a -> String -> O.ParserInfo a
-complete p desc = O.info (O.helper <*> p) 
-                         (mconcat [O.fullDesc, O.progDesc desc])
+info' :: O.Parser a -> String -> O.ParserInfo a
+info' p desc = O.info 
+    (O.helper <*> p) 
+    (mconcat [O.fullDesc, O.progDesc desc])
 
 theParser :: O.Parser Command 
-theParser = O.subparser . mconcat . fmap mksubc $
-        [ ("example", pure Example, "Generate example plan")
-        , ("query", pure Query, "Perform queries") 
-        , ("report", pure Report, "Report on responses") 
-        , ("cat", pure Cat, "Show set of responses") 
-        , ("collate",pure Collate, "Collate two sets of responses") 
-        , ("diff", pure Diff, "Compare two responses") 
-        , ("pretty", pure Pretty, "Print queries") 
-        ] 
+theParser = (O.subparser . mconcat . fmap command') 
+    [ ("example", pure Example, "Generate example plan")
+    , ("query", pure Query, "Perform queries") 
+    , ("report", pure Report, "Report on responses") 
+    , ("cat", pure Cat, "Show set of responses") 
+    , ("collate",pure Collate, "Collate two sets of responses") 
+    , ("diff", pure Diff, "Compare two responses") 
+    , ("pretty", pure Pretty, "Print queries") 
+    ] 
   where 
-    mksubc (name,p,desc) = O.command name (complete p desc)
+    command' (name,p,desc) = O.command name (info' p desc)
 
 main :: IO ()
 main = do
-    O.execParser (complete theParser "This is the main prog desc")
+    O.execParser (info' theParser "This is the main prog desc")
     print "" 
