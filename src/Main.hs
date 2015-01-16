007a2a9ba6f4046e85b32d3412158721408356e1
DDC <diaz_carrete@yahoo.com>
2015-01-16 13:39:06 -0500

    more alternatives

--- src/Main.hs
+++ src/Main.hs
@@ -17,13 +17,22 @@ data Command =
   | Pretty 
   deriving (Show)   
 
+complete :: O.Parser a -> String -> O.ParserInfo a
 complete p desc = O.info (O.helper <*> p) 
-                         (O.fullDesc <> O.progDesc desc)
+                         (mconcat [O.fullDesc, O.progDesc desc])
 
 theParser :: O.Parser Command 
-theParser = (O.subparser . mconcat)
-    [ O.command "example" (complete (pure Example) "Generate example plan")
-    , O.command "query" (complete (pure Query) "Perform queries") ]
+theParser = O.subparser . mconcat . fmap mksubc $
+        [ ("example", pure Example, "Generate example plan")
+        , ("query", pure Query, "Perform queries") 
+        , ("report", pure Report, "Report on responses") 
+        , ("cat", pure Cat, "Show set of responses") 
+        , ("collate",pure Collate, "Collate two sets of responses") 
+        , ("diff", pure Diff, "Compare two responses") 
+        , ("pretty", pure Pretty, "Print queries") 
+        ] 
+  where 
+    mksubc (name,p,desc) = O.command name (complete p desc)
 
 main :: IO ()
 main = do
