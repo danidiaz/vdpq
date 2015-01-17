52ea95417366916247cc27ec340d45602ad27e6c
DDC <diaz_carrete@yahoo.com>
2015-01-17 08:17:15 -0500

    stuff

--- src/Main.hs
+++ src/Main.hs
@@ -17,25 +17,30 @@ data Command =
   | Pretty 
   deriving (Show)   
 
-info' :: O.Parser a -> String -> O.ParserInfo a
-info' p desc = O.info 
-    (O.helper <*> p) 
-    (mconcat [O.fullDesc, O.progDesc desc])
 
-theParser :: O.Parser Command 
-theParser = (O.subparser . mconcat . fmap command') 
-    [ ("example", pure Example, "Generate example plan")
-    , ("query", pure Query, "Perform queries") 
-    , ("report", pure Report, "Report on responses") 
-    , ("cat", pure Cat, "Show set of responses") 
-    , ("collate",pure Collate, "Collate two sets of responses") 
-    , ("diff", pure Diff, "Compare two responses") 
-    , ("pretty", pure Pretty, "Print queries") 
-    ] 
-  where 
+parserInfo' :: O.ParserInfo Command  
+parserInfo' = info' parser' "This is the main prog desc"
+  where
+    parser' :: O.Parser Command 
+    parser' = (O.subparser . foldMap command') 
+        [ ("example", pure Example, "Generate example plan")
+        , ("query", pure Query, "Perform queries") 
+        , ("report", pure Report, "Report on responses") 
+        , ("cat", pure Cat, "Show set of responses") 
+        , ("collate",pure Collate, "Collate two sets of responses") 
+        , ("diff", pure Diff, "Compare two responses") 
+        , ("pretty", pure Pretty, "Print queries") 
+        ] 
+
+    info' :: O.Parser a -> String -> O.ParserInfo a
+    info' p desc = O.info 
+        (O.helper <*> p) 
+        (O.fullDesc <> O.progDesc desc)
+            
     command' (name,p,desc) = O.command name (info' p desc)
+
 
 main :: IO ()
 main = do
-    spec <- O.execParser (info' theParser "This is the main prog desc")
+    spec <- O.execParser parserInfo'
     print spec
